911594c1b24d584b2b1bf212f196dcc0e0e9ebae
gregatgit <gregdd@outlook.com>
2020-02-03 12:45:52 +1000

    put nav bar in home

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    navigation
+    <nav class="site-nave navbar navbar-expand bg-primary navbar-dark">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/home">Home</router-link>
+        <div class="navbar-nav ml-auto">
+          <router-link class="nav-item nav-link" to="/login">LOGOUT</router-link>
+          <router-link class="nav-item nav-link" to=""></router-link>
+          <router-link class="nav-item nav-link" to=""></router-link>
+        </div>
+      </div>
+    </nav>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
+    <Navigation />
     <h1>Welcome</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import Navigation from '../components/Navigation'
 export default {
   name: 'home',
   components: {
-  }
+    Navigation
+  },
 }
 </script>
