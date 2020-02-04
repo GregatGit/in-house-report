0890fb7f299e48e204090f30a918d5e646881c54
gregatgit <gregdd@outlook.com>
2020-02-04 12:59:17 +1000

    add fontawesome

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,36 @@
 <template>
   <div>
     <Navigation />
-    <h1>Welcome</h1>
+    <h1>Welcome <font-awesome-icon icon="star"></font-awesome-icon></h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Navigation from '../components/Navigation'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import db from '../db'
+
 export default {
   name: 'home',
+  data: function() {
+    return {
+      user: null,
+      users: [],
+    }
+  },
+  mounted() {
+    // db.collection('users')
+    //   .get()
+    //   .then(querySnapshot => {
+    //     querySnapshot.forEach(doc => {
+    //       console.log(`${doc.id} => ${doc.data().name}`)
+    //     })
+    //   })
+  },
   components: {
-    Navigation
+    Navigation,
+    FontAwesomeIcon,
   },
 }
 </script>
