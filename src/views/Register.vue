87951353539de290f1cd5e8885c68125ba5c7477
gregatgit <gregdd@outlook.com>
2020-02-05 12:34:12 +1000

    set up basic register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,15 +1,27 @@
 <template>
-  <div>
-
+  <div class="container">
+    <div class="card mt-5">
+      <div class="center text-center">
+        <h5>Hi</h5>
+        <p class="mt- mx-3">Please ask your team leader to sign you up</p>
+        <div class="text-center">
+          <button @click="handleClick" type="button" class="btn " data-toggle="tooltip" data-placement="top" title="back to login">↩</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'register'
+export default {
+  name: 'register',
+  methods: {
+    handleClick: function() {
+      this.$router.push('/login')
+    }
   }
+}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
