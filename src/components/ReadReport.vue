9fc019421981db1932e070d32d44b64a206a67f7
gregatgit <gregdd@outlook.com>
2020-02-07 09:39:31 +1000

    remove comments section (for now)

--- src/components/ReadReport.vue
+++ src/components/ReadReport.vue
@@ -4,30 +4,35 @@
       <u>{{report.title}}</u>
     </h3>
 
-      <div class="list-group-item">
-        <span>By: </span>
-        <span> {{report.by}}</span>
-      </div>
-      <div class="list-group-item">
-        <span>Date issued: </span>
-        <span>{{report.time.date}} {{report.time.hours}}:{{report.time.minutes}}</span>
-      </div>
-      <div class="list-group-item">
-        <span>Difficulty level: </span>
-        <span> {{report.difficulty}}</span>
-      </div>
-      <div class="list-group-item">
-        <span> {{report.description}}</span>
-      </div>
-
-    <button @click="$emit('closeReport')" class="btn btn-outline-success mt-3">CLOSE</button>
+    <div class="list-group-item">
+      <span>By: </span>
+      <span> {{report.by}}</span>
+    </div>
+    <div class="list-group-item">
+      <span>Date issued:</span>
+      <span>{{report.time.date}} {{report.time.hours}}:{{report.time.minutes}}</span>
+    </div>
+    <div class="list-group-item">
+      <span>Difficulty level:</span>
+      <span>{{report.difficulty}}</span>
+    </div>
+    <div class="list-group-item">
+      <span>{{report.description}}</span>
+    </div>
+    <button @click="$emit('closeReport')" class="btn btn-outline-success mt-3">CLOSE</button>   
   </div>
 </template>
 
 <script>
+import db from '../db'
 export default {
   name: 'ReadReport',
   props: ['report'],
+  data: function() {
+    return {
+      messages: [],
+    }
+  }
 }
 </script>
 
